afbd555d827a72863050e581d853069497dcb3f4
yu15645630051 <1417656014@qq.com>
2023-04-05 07:54:12 +0800

    提交代码运管

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,61 +3,78 @@
  * @公众号: 大前端私房菜
 -->
 <template>
-    <div>
-        <el-page-header
-            content="首页"
-            icon=""
-            title="企业门户网站管理系统"
-        />
+  <div>
+    <el-page-header
+      content="首页"
+      icon=""
+      title="企业门户网站管理系统"
+    />
 
-        <el-card class="box-card">
-            <el-row>
-                <el-col :span="4">
+    <el-card class="box-card">
+      <el-row>
+        <el-col :span="4">
 
-                     <el-avatar :size="100" :src="avatarUrl" />
-                    </el-col>
-                <el-col :span="20">
-                    <h3 style="line-height:100px;">欢迎 {{store.state.userInfo.username}} 回来, {{welcomeText}}</h3>
-                </el-col>
-            </el-row>
-        </el-card>
+          <el-avatar
+            :size="100"
+            :src="avatarUrl"
+          />
+        </el-col>
+        <el-col :span="20">
+          <h3 style="line-height:100px;">欢迎 {{store.state.userInfo.username}} 回来, {{welcomeText}}</h3>
+        </el-col>
+      </el-row>
+    </el-card>
 
-        <el-card class="box-card">
-            <template #header>
-                <div class="card-header">
-                    <span>公司产品</span>
+    <el-card class="box-card">
+      <template #header>
+        <div class="card-header">
+          <span>公司产品</span>
 
-                </div>
-            </template>
-            <el-carousel
-                :interval="4000"
-                type="card"
-                height="200px"
-            >
-                <el-carousel-item
-                    v-for="item in 6"
-                    :key="item"
-                >
-                    <h3>{{ item }}</h3>
-                </el-carousel-item>
-            </el-carousel>
-        </el-card>
-    </div>
+        </div>
+      </template>
+      <el-carousel
+        v-if="loopList.length"
+        :interval="4000"
+        type="card"
+        height="200px"
+      >
+        <el-carousel-item
+          v-for="item in loopList"
+          :key="item._id"
+        >
+          <div :style="{backgroundImage: `url(http://localhost:3000${item.cover})`,backgroundSize: 'cover'}">
+
+            <h3>{{ item.title }}</h3>
+          </div>
+        </el-carousel-item>
+      </el-carousel>
+    </el-card>
+  </div>
 </template>
 
 <script setup>
 import { useStore } from "vuex";
-import {computed} from 'vue'
+import { computed, onMounted, ref } from 'vue'
+import axios from 'axios'
 const store = useStore();
+const loopList = ref([])
 // console.log(store.state);
+onMounted(() => {
+  getTableData();
+});
 
+const getTableData = async () => {
+  const res = await axios.get(`/adminapi/product/list`);
+  console.log('首页', res.data.data)
+  loopList.value = res.data.data;
+};
 const avatarUrl = computed(
   () =>
     store.state.userInfo.avatar
-      ? 'http://localhost:3000'+store.state.userInfo.avatar
+      ? 'http://localhost:3000' + store.state.userInfo.avatar
       : `https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png`
 );
-const welcomeText = computed(()=>new Date().getHours()<12?'要开心每一天.':'喝杯咖啡提提神吧.')
+const welcomeText = computed(() => new Date().getHours() < 12 ? '要开心每一天.' : '喝杯咖啡提提神吧.')
 </script>
 <style lang="scss" scoped>
 .box-card {
@@ -65,7 +82,7 @@ const welcomeText = computed(()=>new Date().getHours()<12?'要开心每一天.':
 }
 
 .el-carousel__item h3 {
-  color: #475669;
+  color: #fff;
   font-size: 14px;
   opacity: 0.75;
   line-height: 200px;
@@ -80,5 +97,4 @@ const welcomeText = computed(()=>new Date().getHours()<12?'要开心每一天.':
 .el-carousel__item:nth-child(2n + 1) {
   background-color: #d3dce6;
 }
-
 </style>
